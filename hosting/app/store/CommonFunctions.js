b83669d30dafa8968f874ff1127a9c793c9f90e5
YugaNamba <n.b.y.g.0311.t.n@gmail.com>
2021-08-15 13:30:33 +0900

    feat: 🎸 serialize, getData 追加

--- hosting/app/store/CommonFunctions.js
+++ hosting/app/store/CommonFunctions.js
@@ -14,4 +14,29 @@ export class CommonFunctions {
   static setTimestamp(doc) {
     return CommonFunctions.setCreatedAt(CommonFunctions.updateUpdatedAt(doc))
   }
+
+  static serialize(doc) {
+    const data = doc.data()
+    Object.defineProperty(data, 'id', {
+      value: doc.id,
+      enumerable: true,
+    })
+
+    const fromCache = doc.metadata.fromCache
+    Object.defineProperty(data, 'fromCache', {
+      value: fromCache,
+      enumerable: true,
+    })
+    return data
+  }
+
+  static getData(doc) {
+    if (!doc.exists) {
+      console.log('No such document!')
+      return null
+    }
+    const data = doc.data()
+    data.id = doc.id
+    return data
+  }
 }
